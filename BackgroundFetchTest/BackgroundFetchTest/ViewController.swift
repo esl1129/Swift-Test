326d06ab129edd57c3acf1a563d21537e06045fb
esl1129 <esl112999@gmail.com>
2022-04-20 10:01:53 +0900

    Add Background Fetch Test

---
+++ BackgroundFetchTest/BackgroundFetchTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundFetchTest
+//
+//  Created by mcnc on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
